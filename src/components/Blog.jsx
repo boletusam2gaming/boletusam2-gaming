4b03b3362dcee641020978012feb48aaff8d5b26
Hafiz <ahmed.shiraz89@gmail.com>
2024-11-02 20:58:20 -0500

    updated blog page

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -7,7 +7,7 @@ import { updateTitle } from '../utils/updateTitle';
 import './Blog.css'; // Assuming you have a CSS file for styling
 
 const Blog = () => {
-  const [ setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState('All');
   const [articles, setArticles] = useState([]);
   const [user] = useAuthState(auth);
@@ -31,7 +31,7 @@ const Blog = () => {
     };
 
     fetchPosts();
-  }, []);
+  }, [setPosts]);
 
   const filteredArticles = filter === 'All' ? articles : articles.filter(article => article.category === filter);
 
