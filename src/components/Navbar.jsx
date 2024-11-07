634d113f7bf6d4409d2513a8d572253edd6696a6
Hafiz <ahmed.shiraz89@gmail.com>
2024-11-07 13:11:03 -0600

    added video to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import codVideo from '../assets/mw3_quad_killstreak.mp4';
 import './Navbar.css';
 
 
 export const Navbar = () => {
-  return (
-        <header className="navbar">
-            <div className="logo">
-                <Link to="/">
+return (
+    <header className="navbar">
+        <div className="logo">
+            <Link to="/">
                 <img src={`${process.env.PUBLIC_URL}/images/logo_social_modded.png`} alt="Boletusam2 Gaming logo" />
-                </Link>
-            </div>
-            <nav>
-                <ul>
+            </Link>
+        </div>
+        
+        <nav>
+            <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/blog">Blog</Link></li>
@@ -20,11 +22,13 @@ export const Navbar = () => {
                 <li><Link to="/store">Store</Link></li>
                 <li><Link to="/forum">Forums</Link></li>
                 <li><Link to="/livestream">Live Stream</Link></li>
-
-                </ul>
-            </nav>
-        </header>    
+            </ul>
+            <div className="video">
+                <video src={codVideo} loop muted autoPlay controls></video>
+            </div>
+        </nav>
         
-    );
+    </header>
+);
 }
 export default Navbar;
